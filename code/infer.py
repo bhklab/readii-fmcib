b75b5b60a54422eceac5ddf342b01221f3ac2143
Katy Scott <k.l.scott16@gmail.com>
2024-12-02 17:14:09 -0500

    feat/docs: convert infer into a function, update variable names to be more readable, add docstring for function

--- code/infer.py
+++ code/infer.py
@@ -1,25 +1,47 @@
 from fmcib.run import get_features
 from args import parser
 import os
+from typing import Optional
 
-if __name__ == "__main__":
-    params = parser().parse_args()
+def infer(feature_file_save_path:str, 
+          csv_path:str, 
+          weights_path:Optional[str]=None, 
+          precropped:Optional[bool]=False):
+    """ Function to run FMCIB feature extraction and save out the results to a csv file. 
+
+    Args:
+        feature_file_save_path (str): Path to where to save the output file containing the FMCIB features
+        csv_path (str): Path to the csv file containing the paths to the input images and coordinates
+        weights_path (str): Path to the model weights
+        precropped (bool, optional): Whether to use the precropped data. Defaults to False.
+    """
 
     # make sure the output directory exists
-    dir_path = os.path.dirname(params.save_path)
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
+    feature_dir_path = os.path.dirname(feature_file_save_path)
+    if not os.path.exists(feature_dir_path):
+        os.makedirs(feature_dir_path)
 
     # run FMCIB feature extraction
-    feature_df = get_features(params.input_path, 
-                              weights_path=params.weights_path, 
-                              precropped=params.precropped)
+    feature_df = get_features(csv_path=csv_path, 
+                              weights_path=weights_path, 
+                              precropped=precropped,
+                              spatial_size=(50,50,50))
     
     # clean up the dataframe
     feature_df.drop(columns=['coordX', 'coordY', 'coordZ'], inplace=True)
     
-    # save the FMCIB features to `params.save_path`
-    feature_df.to_csv(params.save_path, index=False)
+    # save out the FMCIB features to a csv
+    feature_df.to_csv(feature_file_save_path, index=False)
     
     # sanity check to STDOUT
-    print(feature_df)
+    # print(feature_df)
+    return feature_df
+
+
+if __name__ == "__main__":
+    params = parser().parse_args()
+
+    infer(feature_file_save_path=params.save_path, 
+          csv_path=params.input_path, 
+          weights_path=params.weights_path, 
+          precropped=params.precropped)
