ada1a18f5f38e425e996a628ad0a1188eafa734c
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-01-03 17:49:20 +0000

    add negative control loop for inference

--- code/infer.py
+++ code/infer.py
@@ -10,16 +10,18 @@ if __name__ == "__main__":
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
+    neg_controls = [f"{neg_method}_{neg_area}" for neg_method in ["shuffled"] for neg_area in ["full", "non_roi", "roi"]]
+    neg_controls += ["original"]
     # run FMCIB feature extraction
-    feature_df = get_features(params.input_path, 
-                              weights_path=params.weights_path, 
-                              precropped=params.precropped)
+    for neg_control in neg_controls:
+        feature_df = get_features(params.input_path.replace(".csv", f"_{neg_control}.csv"), 
+                                  precropped=params.precropped)
     
-    # clean up the dataframe
-    feature_df.drop(columns=['coordX', 'coordY', 'coordZ'], inplace=True)
-    
-    # save the FMCIB features to `params.save_path`
-    feature_df.to_csv(params.save_path, index=False)
+        # clean up the dataframe
+        feature_df.drop(columns=['coordX', 'coordY', 'coordZ'], inplace=True)
+        
+        # save the FMCIB features to `params.save_path`
+        feature_df.to_csv(params.save_path.replace(".csv", f"_{neg_control}.csv"), index=False)
     
     # sanity check to STDOUT
     print(feature_df)
