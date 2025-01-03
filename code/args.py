e01bcb9624d0fc071c49d7ac4111b99c3dd21103
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-01-03 17:48:48 +0000

    update input arguments

--- code/args.py
+++ code/args.py
@@ -6,9 +6,9 @@ dotenv.load_dotenv()
 
 def parser():
     parser = ArgumentParser()
-    parser.add_argument('--input_path', type=str, default=os.environ["DATA_PATH"], help='Path to the CSV with the data')
-    parser.add_argument('--save_path', type=str, default="data/fmcib.csv", help='Path to the output directory')  
-    parser.add_argument('--weights_path', type=str, default=os.environ["MODEL_PATH"], help='Path to the model weights')
+    parser.add_argument('--input_path', type=str, default=os.environ["SAVE_PATH"], help='Path to the CSV with the data')
+    parser.add_argument('--save_path', type=str, default=os.environ["FMCIB_SAVE_PATH"], help='Path to the output directory')  
+    parser.add_argument('--weights_path', type=str, default=None, help='Path to the model weights')
     parser.add_argument('--precropped', action='store_true', help='Whether to use the precropped data')
     parser.parse_args()
 
@@ -26,6 +26,6 @@ def readii_parser():
     parser.add_argument('base_path', type=str, help='Path to the base directory')
     parser.add_argument('--input_format', type=str, help='Path to the input directory and regex pattern')
     parser.add_argument('--mask_format', type=str, help='Path to the mask and regex pattern')
-    parser.add_argument('--output_path', type=str, default=os.environ["ZOOM_PATH"], help='Path to the output directory')
-    parser.add_argument('--save_path', type=str, default=os.environ["DATA_PATH"], help='Path to the output file')
+    parser.add_argument('--output_path', type=str, default=None, help='Path to the output directory')
+    parser.add_argument('--save_path', type=str, default=None, help='Path to the output file')
     return parser
